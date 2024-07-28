5aa46782a1b767cc1badfacc63e26a755cbe2408
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-28 08:57:40 +0300

    add admin register

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing.models import Settings, Attempt
+
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ('first_mailing_date', 'get_client', 'status',)
+    list_filter = ('first_mailing_date', 'status',)
+
+    def get_client(self, obj):
+        return ', '.join([p.email for p in obj.client.all()])
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('last_attempt_date', 'status',)
+    list_filter = ('last_attempt_date', 'status',)
