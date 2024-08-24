b162b77bd56052e4fef7054ffb08a01cab9a420a
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-24 23:08:14 +0300

    fix settings.py заработала регистрация

--- config/settings.py
+++ config/settings.py
@@ -139,13 +139,13 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/users/login/"
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-
-EMAIL_HOST_USER = "olonovaleksandar@yandex.ru"
-EMAIL_HOST_PASSWORD = "wpaqnubuejmchlrk"
+EMAIL_HOST_USER = 'olonovaleksandar@yandex.ru'
+EMAIL_HOST_PASSWORD = 'wjtrwplrrcucbtdm'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
