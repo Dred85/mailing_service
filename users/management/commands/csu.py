ff94ca72348db26818117876c7a0ac3fc36012d5
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-24 22:19:24 +0300

    fix

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -6,7 +6,7 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email="adrolv@rumbler.ru",
+            email="admin@rambler.ru",
             first_name="Admin",
             last_name="Skypro",
             is_staff=True,
