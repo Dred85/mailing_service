57fafb8db704e35d6086b76e4d75b92f9deee089
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-25 21:15:47 +0300

    Отобразил 3 случайных статьи

--- client/templatetags/my_tags.py
+++ client/templatetags/my_tags.py
@@ -6,7 +6,11 @@ import string
 
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
+
+from blog.models import Blog
+from client.models import Client
 from mailing.models import Settings
+
 register = template.Library()
 
 
@@ -19,8 +23,33 @@ def current_time(format_string):
 @register.simple_tag
 def count_mailing():
     """ Подсчет общего количества рассылок"""
-    count_mailing = Settings.objects.count()
-    return f"У нас самый лучший сервис! Количество рассылок сейчас {count_mailing}"
+    count_mailing_ = Settings.objects.count()
+    return f"Количество рассылок сейчас: {count_mailing_}"
+
+
+@register.simple_tag
+def count_mailing_activ():
+    """ Подсчет количества активных рассылок"""
+    count_mailing_activ_ = Settings.objects.filter(is_active=True).count()
+    return f"Количество активных рассылок сейчас: {count_mailing_activ_}"
+
+
+@register.simple_tag
+def count_clients_uniq():
+    """ Подсчет количества уникальных клиентов для рассылок"""
+    count_clients_uniq_ = Client.objects.values('email').distinct().count()
+    return f"Количество уникальных клиентов для рассылок: {count_clients_uniq_}"
+
+
+@register.simple_tag
+def get_random_3_blogs():
+    """Возвращает три случайные статьи из блога."""
+    all_blogs = list(Blog.objects.all())
+    if len(all_blogs) < 3:
+        return all_blogs  # Возвращаем все статьи, если их меньше трех
+
+    random_blogs_list = random.sample(all_blogs, 3)
+    return random_blogs_list
 
 
 
@@ -48,16 +77,6 @@ def generate_fake_mail(length: int = "10"):
     return f"{mail}@{mail2}.com"
 
 
-# Создание фильтра
-@register.filter
-def last_3_blogs(query_set):
-    number = len(query_set)
-    if number <= 3:
-        return query_set
-    else:
-        return query_set[number - 3 : number + 1]
-
-
 @register.filter()
 def media_filter(path):
     if path:
@@ -69,3 +88,19 @@ def media_filter(path):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+# @register.filter
+# def get_random_3_blogs(query_set):
+#     """Возвращает три случайные статьи из блога."""
+#
+#     # Получаем все статьи
+#     all_blogs = query_set
+#     # Проверяем, достаточно ли статей
+#     if len(all_blogs) < 3:
+#         return all_blogs  # Возвращаем все статьи, если их меньше трех
+#
+#     # Выбираем три случайные статьи
+#     random_blogs_list = random.sample(all_blogs, 3)
+#
+#     return f" три случайные блога {random_blogs_list}"
