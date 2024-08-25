9f0290afbdcd7b6dff1cf10b7cf4a3a90aa7073b
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-25 06:41:20 +0300

    добавил owner и  permissions в модель Message

--- message/models.py
+++ message/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 
+from users.models import User
+
 
 class Message(models.Model):
     subject = models.CharField(max_length=150, verbose_name='тема письма')
     text = models.TextField(verbose_name='текст сообщения')
+    owner = models.ForeignKey(
+        User,
+        verbose_name="владелец",
+        help_text="Укажите владельца сообщения",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
 
-    def __str__(self):
-        return f'{self.subject}'
+
 
     class Meta:
         verbose_name = 'сообщение'
         verbose_name_plural = 'сообщения'
+        permissions = [
+            ("can_edit_subject", "can_edit_subject"),
+            ("can_edit_text", "can_edit_text"),
+        ]
+
+    def __str__(self):
+        return f'{self.subject}'
