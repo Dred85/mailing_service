8832ce6b0bc1c30a8ec50c1d194f19c05ae0e82e
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-30 19:26:17 +0300

    fix auth message

--- mailing/views.py
+++ mailing/views.py
@@ -41,52 +41,23 @@ class SettingsCreateView(LoginRequiredMixin, CreateView):
         return reverse('mailing:settings_list')
 
 
-class SettingsUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+class SettingsUpdateView(LoginRequiredMixin,  UpdateView):
     model = Settings
     form_class = SettingsForm
 
-    permission_required = (
-        "mailing.can_view_mailing",
-        "mailing.can_view_users",
-        "mailing.can_blocked_users",
-        "mailing.can_disabled_mailing",
-    )
     extra_context = {
         'title': 'Форма по редактированию'
     }
 
-    def get_form_class(self):
-        user = self.request.user
-        if user == self.object.owner:
-            return SettingsForm
-        if user.has_perm("can_view_mailing") and user.has_perm("can_view_users") and user.has_perm(
-                "can_blocked_users") and user.has_perm("can_disabled_mailing"):
-            return MailingModeratorForm
-        raise PermissionDenied
 
     def get_success_url(self):
         return reverse('mailing:settings_detail', args=[self.kwargs.get('pk')])
 
 
-class SettingsDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+class SettingsDeleteView(LoginRequiredMixin, DeleteView):
     model = Settings
     form_class = SettingsForm
 
-    permission_required = (
-        "mailing.can_view_mailing",
-        "mailing.can_view_users",
-        "mailing.can_blocked_users",
-        "mailing.can_disabled_mailing",
-    )
-
-    def get_form_class(self):
-        user = self.request.user
-        if user == self.object.owner:
-            return SettingsForm
-        if user.has_perm("can_view_mailing") and user.has_perm("can_view_users") and user.has_perm(
-                "can_blocked_users") and user.has_perm("can_disabled_mailing"):
-            return MailingModeratorForm
-        raise PermissionDenied
 
     extra_context = {
         'title': 'Удаление рассылки'
