479de5da0607067f9b34bc76bc781eb9cf7bd9d5
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-30 21:57:27 +0300

    добавил  owner в admin client

--- client/admin.py
+++ client/admin.py
@@ -6,4 +6,4 @@ from client.models import Client
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'owner')
-    list_filter = ('first_name', 'last_name', 'email',)
+    list_filter = ('first_name', 'last_name', 'email','owner')
