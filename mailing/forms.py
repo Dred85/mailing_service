316b5d4098841c9f1ab3352cc6f9fe59d9cb9411
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-01 08:46:40 +0300

    fix forms.py: теперь в форме отображаются сообщения и клиенты, только пользователя

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from client.models import Client
 from .mixins import StyledFormMixin
 from .models import Settings
 
@@ -7,10 +8,29 @@ from .models import Settings
 class SettingsForm(forms.ModelForm):
     class Meta:
         model = Settings
+        user = forms.ChoiceField(required=True)
         fields = ["first_mailing_date",
-                  "frequency", "status", "message", "client", "owner",  "is_active"]
+                  "frequency", "status", "message", "client", "owner", "is_active"]
+
 
 class MailingModeratorForm(StyledFormMixin, forms.ModelForm):
     class Meta:
         model = Settings
         fields = ["is_active"]
+
+
+class MailingModeratorFormOwner(StyledFormMixin, forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)  # Извлекаем пользователя из аргументов
+        super(MailingModeratorFormOwner, self).__init__(*args, **kwargs)
+
+        if user:
+            # Фильтруем клиентов по текущему пользователю
+            self.fields['client'].queryset = Client.objects.filter(owner=user)
+            self.fields['message'].queryset = Client.objects.filter(owner=user)
+
+    class Meta:
+        model = Settings
+        fields = '__all__'  # Используйте '__all__', чтобы указать все поля
+
